9e7fa8688a4a2d7edddab9605382e997ef729df9
chanin <pci2676@gmail.com>
2019-12-13 19:21:17 +0900

    LSP - feat modify isp test code
     - modify good case test code

--- CleanCodePost/solid/src/test/java/solid/isp/after/GoodIspTest.java
+++ CleanCodePost/solid/src/test/java/solid/isp/after/GoodIspTest.java
@@ -7,7 +7,6 @@ import solid.isp.after.copy.CopyMachine;
 import solid.isp.after.fax.FaxDevice;
 import solid.isp.after.fax.FaxMachine;
 import solid.isp.after.printer.PrinterDevice;
-import solid.isp.after.printer.PrinterMachine;
 
 class GoodIspTest {
 
@@ -23,9 +22,21 @@ class GoodIspTest {
     @DisplayName("하나의 기능만을 필요로 한다면 하나의 인터페이스만 구현하도록 하자")
     @Test
     void singleInterface() {
-        PrinterDevice printer = new PrinterMachine();
+        PrinterDevice printer = new SmartMachine();
         FaxDevice faxMachine = new FaxMachine();
         CopyDevice copyMachine = new CopyMachine();
+
+        printer.print();
+        faxMachine.fax();
+        copyMachine.copy();
+    }
+
+    @DisplayName("특정 기능만 클라이언트에게 노출시킬수 있다.")
+    @Test
+    public void singleFunction() {
+        PrinterDevice printer = new SmartMachine();
+        FaxDevice faxMachine = new SmartMachine();
+        CopyDevice copyMachine = new SmartMachine();
 
         printer.print();
         faxMachine.fax();
