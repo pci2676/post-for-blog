10e709b33f20d8f3a44ab5f689bdb247c725e913
chanin <pci2676@gmail.com>
2020-01-23 15:54:43 +0900

    아이템 7

--- JavaPost/effective/example/src/main/java/chap2/item7/MemoryLeak.java
+++ JavaPost/effective/example/src/main/java/chap2/item7/MemoryLeak.java
@@ -1,6 +1,23 @@
 package chap2.item7;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class MemoryLeak {
+
+    public static void main(String[] args) {
+        Map<Foo, String> map = new HashMap<>();
+
+        Foo key = new Foo();
+        map.put(key, "1");
+
+        key = null;
+        System.gc();
+
+        Scanner sc = new Scanner(System.in);
+        sc.nextLine();
+    }
 }
 
 class Foo {
