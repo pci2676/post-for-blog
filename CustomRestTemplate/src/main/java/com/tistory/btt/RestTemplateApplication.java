df0eb022ed07ed0733af6029ff50b05c531041d6
chanin <pci2676@gmail.com>
2020-01-28 14:54:15 +0900

    init

--- CustomRestTemplate/src/main/java/com/tistory/btt/RestTemplateApplication.java
+++ CustomRestTemplate/src/main/java/com/tistory/btt/RestTemplateApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BttApplication {
+public class RestTemplateApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BttApplication.class, args);
+        SpringApplication.run(RestTemplateApplication.class, args);
     }
 
 }
--- CustomRestTemplate/src/test/java/com/tistory/btt/RestTemplateApplicationTests.java
+++ CustomRestTemplate/src/test/java/com/tistory/btt/RestTemplateApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class BttApplicationTests {
+public class RestTemplateApplicationTests {
 
     @Test
     public void contextLoads() {
