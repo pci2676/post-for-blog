5031954d435b41bea800ee993bc794426c8c1aa4
chanin <pci2676@gmail.com>
2020-02-13 09:48:02 +0900

    restdocs example project init

---
+++ restdocs/src/main/java/com/tistory/javabom/RestdocsApplication.java
@@ -0,0 +1,13 @@
+package com.tistory.javabom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestdocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestdocsApplication.class, args);
+	}
+
+}
---
+++ restdocs/src/test/java/com/tistory/javabom/RestdocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tistory.javabom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestdocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
