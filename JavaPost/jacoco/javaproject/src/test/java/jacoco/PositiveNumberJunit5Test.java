d8fbf4e93cba7379ff6bc6a0982ca26155ac2616
chanin <pci2676@gmail.com>
2020-02-29 03:04:56 +0900

    mix junit4 and junit5

--- JavaPost/jacoco/javaproject/src/test/java/jacoco/PositiveNumberJunit5Test.java
+++ JavaPost/jacoco/javaproject/src/test/java/jacoco/PositiveNumberJunit5Test.java
@@ -1,16 +1,35 @@
 package jacoco;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class PositiveNumberJunit5Test {
 
+    @DisplayName("양수 생성")
     @Test
-    void notPositiveValueThrowException() {
-        long notPositive = 0;
+    void createTest() {
+        long positive = 1;
+        PositiveNumber positiveNumber = new PositiveNumber(positive);
 
-        assertThatThrownBy(() -> new PositiveNumber(notPositive))
-                .isInstanceOf(IllegalArgumentException.class);
+        assertThat(positiveNumber).isEqualTo(new PositiveNumber(1));
+    }
+
+    @DisplayName("양수 덧셈")
+    @Test
+    void addTest() {
+        //given
+        PositiveNumber result = new PositiveNumber(2);
+
+        PositiveNumber positiveNumber1 = new PositiveNumber(1);
+        PositiveNumber positiveNumber2 = new PositiveNumber(1);
+
+        //when
+        PositiveNumber expect = positiveNumber1.add(positiveNumber2);
+
+
+        //then
+        assertThat(expect).isEqualTo(result);
     }
 }
