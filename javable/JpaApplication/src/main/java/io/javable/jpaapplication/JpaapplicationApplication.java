074fad0a10057a910d703e5a5e671f2c3199769d
chanin <pci2676@gmail.com>
2020-08-31 22:46:54 +0900

    project init

---
+++ javable/JpaApplication/src/main/java/io/javable/jpaapplication/JpaapplicationApplication.java
@@ -0,0 +1,13 @@
+package io.javable.jpaapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaapplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaapplicationApplication.class, args);
+    }
+
+}
---
+++ javable/JpaApplication/src/test/java/io/javable/jpaapplication/JpaapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javable.jpaapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaapplicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
