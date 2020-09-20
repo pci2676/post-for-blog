6509092c8811a35f776dac4b776ae99edf728a3e
chanin <pci2676@gmail.com>
2020-09-20 17:21:27 +0900

    project init

---
+++ SpringPost/ymlpropertiesbinding/src/test/java/com/javabom/ymlpropertiesbinding/YmlpropertiesbindingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javabom.ymlpropertiesbinding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YmlpropertiesbindingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
